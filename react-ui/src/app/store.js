e03e8ad04f43fdb4d88df5d226b5119e2732943f
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-05-23 18:29:45 -0700

    trying to create allbreeds function

--- react-ui/src/app/store.js
+++ react-ui/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import breedsReducer from '../features/breedsSlice';
+// import subBreedsReducer from '..features/breedsSlice';
 import selectedBreedsReducer from '../features/selectedBreedsSlice';
 
 export default configureStore({
   reducer: {
     breeds: breedsReducer,
+    // breedsWithSubbreeds: subBreedsReducer,
     selectedBreeds: selectedBreedsReducer,
   }
 });
--- react-ui/src/features/breedsSlice.js
+++ react-ui/src/features/breedsSlice.js
@@ -1,8 +1,25 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+// const findAllBreeds = data => {
+//     const breeds = Object.keys(data)
+//     const subBreedsArray = []
+//     const currentBreedNameIndex = " ";
+
+//     for(let i=0; i < breeds.length; i++) {
+//       if(breeds[i].length > 0 ) {
+//         for(let j=0; j < breeds[i]; j++) {
+//           subBreedsArray.push(`${breeds[i]}-${breeds[j]}`)
+//         }
+//       }
+//     }
+//     return [...breeds, ...subBreedsArray];
+// };
+
 export const fetchBreeds = createAsyncThunk('breeds/all', async() => {
   const response = await fetch('https://dog.ceo/api/breeds/list/all');
   const data = await response.json();
+  const allBreeds = findAllBreeds(data.message)
+  console.log(allBreeds)
   const breeds = Object.keys(data.message)
   return breeds
 })
