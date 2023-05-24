a5a5bb172a6f7db1c3adb1d5c7b99850527aeee0
christinab980 <71195516+christinab980@users.noreply.github.com>
2023-05-23 18:04:27 -0700

    added fetch call for quiz

--- react-ui/src/views/quiz.jsx
+++ react-ui/src/views/quiz.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Quiz = () => {
+  const [options, setOptions] = useState([])
+  const [answer, setAnswer] = useState([]);
+
+  useEffect(() => {
+
+  const fetchData = async () => {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random/4')
+    const data = await response.json()
+    setOptions(data.message) 
+  }
+    fetchData()
+  
+}, []);
+
+  const images = options.map((option) => {
+        return (
+        <div key={option}> 
+        <img src={option} alt="dog img"/>
+        </div>
+        )
+  })
+
+  
+
   return (
+    <>
     <h1>Quiz</h1>
+
+    <div className='dog-imgs'> {images} </div>
+    </>
   )
-};
+}
+
 
 export default Quiz;
